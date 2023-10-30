afdf695517a679302853c0769e86d409c2e63099
LittlePistol <Foxwill.kndst@gmail.com>
2023-10-30 12:22:05 +0300

    Начало Firebase c кучей ошибок

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'pages/favorite_page.dart';
 import 'pages/generator_page.dart';
