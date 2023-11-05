26b1c1fe978470049299123731d54c5dc105ca1c
LittlePistol <Foxwill.kndst@gmail.com>
2023-11-05 19:55:54 +0300

    Login Error messages and loading indicator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,19 +4,127 @@ import 'package:namer_app_web/Components/my_Textfield.dart';
 import 'package:namer_app_web/components/my_button.dart';
 import 'package:namer_app_web/components/square_tile.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   // text editing controllers
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   // sign user in method
-
   void signUserIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: emailController.text,
-      password: passwordController.text,
+    // show a loading circle
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    //try sign user in
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      //pop the circle
+
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      //USER NOT FOUND
+      if (e.code == 'user-not-found') {
+        //show error to user
+        userNotFoundMessage();
+        print('No user found with this email');
+      }
+      //SOMETHINGS WHONG
+      else if (e.code == 'invalid-login-credentials') {
+        wrongSomethingMessage();
+        print('Check everything again');
+      }
+      //INVALID EMAIL
+      else if (e.code == 'invalid-email') {
+        wrongEmailMessage();
+        print('You typed email wrong');
+      }
+      //WRONG PASSWORD
+      else if (e.code == 'wrong-password') {
+        wrongPasswordMessage();
+        print('Password is wrong, buddy');
+      }
+      //I DUNNO MAN
+      else {
+        otherErrorMessage();
+        print(e.code);
+      }
+    }
+  }
+
+  //USER NOT FOUND message popup
+  void userNotFoundMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('User not found'),
+        );
+      },
+    );
+  }
+
+  //SOMETHINGS WRONG message popup
+  void wrongSomethingMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Check your email and password'),
+        );
+      },
+    );
+  }
+
+  //IVALID EMAIL message popup
+  void wrongEmailMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Invalid Email'),
+        );
+      },
+    );
+  }
+
+  // WRONG PASSWORD message popup
+  void wrongPasswordMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('Incorrect password'),
+        );
+      },
+    );
+  }
+
+  //OTHER error message popup
+  void otherErrorMessage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          title: Text('I dont know man'),
+        );
+      },
     );
   }
 
